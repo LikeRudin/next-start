f3a131513111bf5b5a2863b897ff2c305e170d2d
Young Jin <likeRudin@gmail.com>
2023-10-15 01:24:43 +0900

    bugfix: handle prop issue

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default function IndexPage({ results, copyright }) {
           {info["display_name"]} &rArr;
         </Link>
       ))}
+      <h3>{copyright}</h3>
     </>
   );
 }
@@ -18,7 +19,6 @@ export async function getServerSideProps() {
     await fetch("https://books-api.nomadcoders.workers.dev/lists")
   ).json();
   return {
-    props: results,
-    copyright,
+    props: { results, copyright },
   };
 }
